b19d43d74cb184fccebd5eaff85e13a63ec462aa
hexianglong <112679489+3234374354@users.noreply.github.com>
2025-05-10 09:10:22 +0000

    feat: 更新AssemblyInfo.cs中的版本信息

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,8 +1,8 @@
 using System.Reflection;
 using System.Runtime.Versioning;
 
-[assembly: AssemblyVersion(ThisAssembly.Git.BaseTag)]
-[assembly: AssemblyInformationalVersion($"{ThisAssembly.Git.BaseTag}+{ThisAssembly.Git.Sha}")]
+[assembly: AssemblyVersion("10.0.0.0")] // Replace with the appropriate version
+[assembly: AssemblyInformationalVersion("10.0.0.0+commit-hash")] // Replace with the appropriate version and commit hash
 [assembly: AssemblyTitle("ClassIsland")]
 [assembly: AssemblyProduct("ClassIsland")]
 #if NETCOREAPP
