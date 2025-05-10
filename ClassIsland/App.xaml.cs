fa05cf4ccfc678bbe5b1e41d9f5e8a0aec9e4499
hexianglong <112679489+3234374354@users.noreply.github.com>
2025-05-10 09:16:54 +0000

    feat: 添加对System.Windows.Controls.Primitives的引用

--- ClassIsland/App.xaml.cs
+++ ClassIsland/App.xaml.cs
@@ -18,6 +18,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Threading;
+using System.Windows.Controls.Primitives;
 
 using ClassIsland.Controls;
 using ClassIsland.Controls.AttachedSettingsControls;
